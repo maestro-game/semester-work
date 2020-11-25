f61a8ed404a1647899cfe56db03336bf09d81eed
Damir <damir17022000@gmail.com>
2020-11-26 00:54:07 +0300

    add search

--- src/main/resources/js/post.js
+++ src/main/resources/js/post.js
@@ -7,4 +7,12 @@ window.addEventListener("load", () =>{
         desc.background = "#228b5e";
         desc.color = "white";
     });
+
+    document.querySelector(".description__change").addEventListener("click", () => {
+        let neededNode = document.querySelector(".description");
+        let form = document.createElement("form");
+        form.action = "/post";
+        form.id = ""
+        // neededNode
+    })
 });
